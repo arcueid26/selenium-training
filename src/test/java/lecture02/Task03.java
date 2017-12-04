c6517f6fa92439f75885a0d660286400f0af9a59
vgulidova <vg#rgs.digital>
2017-12-04 15:17:56 +0300

    Added Task03 class

---
+++ src/test/java/lecture02/Task03.java
@@ -0,0 +1,37 @@
+package lecture02;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class Task03 {
+
+    private String username = "admin";
+    private String password = "admin";
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @BeforeTest
+    public void start() {
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 10);
+    }
+
+    @Test
+    public void loginToAdminArea() {
+        driver.get("http://localhost/litecart/admin/login.php");
+        driver.findElement(By.name("username")).sendKeys(username);
+        driver.findElement(By.name("password")).sendKeys(password);
+        driver.findElement(By.name("login")).click();
+    }
+
+    @AfterTest
+    public void stop() {
+        driver.quit();
+        driver = null;
+    }
+}
